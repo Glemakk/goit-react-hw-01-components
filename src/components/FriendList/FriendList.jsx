8d6ef7052cda5a773d3aef4cf0a5801268aa0f76
Glemakk <kovgar.brand@gmail.com>
2021-07-01 10:49:30 +0200

    Добавил в комментах friends как можно перенести li по желанию ментора

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -16,3 +16,17 @@ const FriendList = ({ friends }) => (
 )
 
 export default FriendList
+
+//------------------Ментор предложила переделать так
+// const FriendList = ({ friends }) => (
+// <ul className={s.list}>
+// {friends.map((friend) => (
+// <FriendListItem
+//    key={friend.id}
+//    avatar={friend.avatar}
+//    name={friend.name}
+//    status={friend.isOnline}
+// />
+// ))}
+// </ul>
+// )
--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -17,3 +17,14 @@ FriendListItem.propTypes = {
   name: PropTypes.string.isRequired,
   status: PropTypes.bool.isRequired,
 }
+
+//--------Ментор предложила сделать так
+// export default function FriendListItem({ avatar, name, status }) {
+//   return (
+//     <li className="item">
+//       <span className={status ? s.onLine : s.offLine}></span>
+//       <img className={s.avatar} src={avatar} alt="" width="48" />
+//       <p className="name">{name}</p>
+//     </li>
+//   )
+// }
